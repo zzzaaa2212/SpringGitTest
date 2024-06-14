afd38881498411ff80efbaa257b22d463fc6a972
user1 <user1@DESKTOP-IVNGPKC>
2024-06-14 11:01:12 +0900

    바꿔봄

--- Test2_git/src/main/java/com/kh/git1/HomeController.java
+++ Test2_git/src/main/java/com/kh/git1/HomeController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+	//내용바꿔봄
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
